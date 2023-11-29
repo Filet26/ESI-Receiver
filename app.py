85d8f3142ed9378afd2af3ef51820425558d6fce
Desmond Ho <desmondh268@gmail.com>
2023-11-29 12:48:17 -0800

    changed app

--- app.py
+++ app.py
@@ -151,10 +151,13 @@ def get_healthcheck():
 
 # app config
 app = connexion.FlaskApp(__name__, specification_dir="./")
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+app.add_api(
+    "openapi.yaml",
+    base_path="/receiver",
+    strict_validation=True,
+    validate_responses=True,
+)
 
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
-
-# hehed
